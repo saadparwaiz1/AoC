f3b29a36d0ae8761d2256ff9c4f2d0ff53a8e2f3
Saad Parwaiz <saad.parwaiz1@gmail.com>
2020-12-12 21:12:28 +0000

    Added Part 1 Solution For Day 8

---
+++ eight-day/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func loop_start() int {
+	var m map[int]bool
+	m = make(map[int]bool)
+	acc := 0
+
+	var lines, _ = readLines("data/input")
+
+	for i := 0; i < len(lines); i++ {
+		if m[i] {
+			break
+		} else {
+			string_array := strings.Split(lines[i], " ")
+			instruction := string_array[0]
+			value, _ := strconv.Atoi(string_array[1])
+
+			if instruction == "acc" {
+				acc += value
+			}
+
+			if instruction == "nop" {
+				continue
+			}
+
+			if instruction == "jmp" {
+				m[i] = true
+				i += value
+				i -= 1
+			} else {
+				m[i] = true
+			}
+
+		}
+	}
+
+	return acc
+}
+
+func main() {
+	acc := loop_start()
+	fmt.Println(acc)
+}
